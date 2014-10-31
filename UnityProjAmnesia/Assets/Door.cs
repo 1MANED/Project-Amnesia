5d174f79482ab2ae8c04c1533bc5606b00bcd924
andreev1995 <skarlet32732@gmail.com>
2014-10-31 20:19:57 +0300

    Новая анимация двери и скрипт

---
+++ UnityProjAmnesia/Assets/Door.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+	public bool Open;
+	public GameObject Player;
+	public float Distance_To_Door;
+	// Use this for initialization
+	void Start () 
+	{
+		Player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{	
+		if(Input.GetKeyDown(KeyCode.E) & 
+		   Vector3.Distance(transform.position, player.transform.position)<Distance_To_Door)
+		{
+			Open = !Open;
+			if(Open==true)
+			{
+				transform.animation.Play("Open_Door");
+			}
+			else
+			{
+				transform.animation.CrossFade("Close_Door");
+			}
+		}
+	}		
+}
---
+++ UnityProjAmnesia/Assets/Open.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Open : MonoBehaviour {
+	public bool open;
+	public GameObject player;
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindGameObjectsWithTag("Player")[0];
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		   if(Input.GetKeyDown(KeyCode.E) & 
+		   		Vector3.Distance(transform.position, player.transform.position)<2)
+			{
+			open = !open;
+			if(open==true)
+			{
+				//transform.anim
+				transform.animation.Play("Open_Door");
+				//a_open.Play();
+			}
+			else
+			{
+				transform.animation.CrossFade("Close_Door");
+			}
+		}
+	}		
+}
